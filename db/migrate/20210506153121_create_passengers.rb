2c8b7e08297947973b1ebea4409ad7377ed32162
maryanne-rose <rosemary.njeri@student.moringaschool.com>
2022-10-01 16:32:07 +0300

    commit tree cc7c304d099c0356cf410e0c9a6d3f0d8540f225

--- db/migrate/20210506153121_create_passengers.rb
+++ db/migrate/20210506153121_create_passengers.rb
@@ -1,7 +1,9 @@
 class CreatePassengers < ActiveRecord::Migration[6.1]
   def change
     create_table :passengers do |t|
-
+      t.string :name
+      t.string :age
+      t.string :gender
       t.timestamps
     end
   end
--- db/migrate/20210506153127_create_rides.rb
+++ db/migrate/20210506153127_create_rides.rb
@@ -1,7 +1,10 @@
 class CreateRides < ActiveRecord::Migration[6.1]
   def change
     create_table :rides do |t|
-
+      t.string :departure_location
+      t.string :arrival_location
+      t.integer :taxi_id
+      t.integer :passenger_id
       t.timestamps
     end
   end
