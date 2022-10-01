c3d2ad33cd176c959b1d9ff54a57019233c6d299
maryanne-rose <rosemary.njeri@student.moringaschool.com>
2022-10-01 16:33:57 +0300

    AR associations review lab

--- db/migrate/20210506153111_create_taxis.rb
+++ db/migrate/20210506153111_create_taxis.rb
@@ -1,7 +1,8 @@
 class CreateTaxis < ActiveRecord::Migration[6.1]
   def change
     create_table :taxis do |t|
-
+      t.string :license_name
+      t.string :car_type
       t.timestamps
     end
   end
